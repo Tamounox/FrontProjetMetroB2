0958fa1d24ef4e40d62fef00e5beb39506b72870
Tamounox <antoine.gaston@ynov.com>
2018-06-11 16:57:38 +0200

    page stats

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StatsComponent } from './stats/stats.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'stats',
+    component: StatsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { StatsComponent } from './stats/stats.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    StatsComponent
   ],
   imports: [
     MDBBootstrapModule.forRoot(),
